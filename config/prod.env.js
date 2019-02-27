822fa339a07d1a512aa6762050fd20f209dcd89c
yexiangyang <yexiangyang@cn.wilmar-intl.com>
2019-02-27 13:31:37 +0800

    可配置文件上传服务

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  FILE_UPLOAD_API: '"http://peer:1443/upload"',
+  FILE_UPLOAD_API: '"http://www.whymoy.com:1443/upload"',
   BASE_API: '"http://www.whymoy.com:8443"'
 }
